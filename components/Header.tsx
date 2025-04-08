353cbb6e4db3ee3027d1259580d0addd67ccc7e0
Arya Dasgupta <aryadasgupta2004@gmail.com>
2025-04-08 21:25:39 +0530

    made chnages to the web3auth api

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
 import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { createUser, getUnreadNotifications, markNotificationAsRead, getUserByEmail, getUserBalance } from "@/utils/db/actions"
 
-const clientId = "BJKdDFkNtkWX87XqkuWrDu4rbkSvWyQZ5lswS0ucINxxcN0inRVW8zzKAywPPzgiOHP7_3PcfFwfpvcQvSdaLRs";
+const clientId = "BAg5SeUF5_78JBR2wXZogBX3uQi77ENl5cVeeLpfwDrg7EN3FkXBcefOAUWnW7BP5VSo9L8JkknVw_WIY5XN4F4";
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
